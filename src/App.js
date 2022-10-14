5f14f21c263f016dadc728283fb7a30b75475e7b
icyhot12 <rmierniczek1@gmail.com>
2022-10-14 23:18:17 +0200

    adding logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from "react";
 import Square from "./Square";
 
+
 function App() {
+  const [squareElements, setSquareElements] = React.useState([])
+
+  for (let i = 0; i < 10; i++) {
+    setSquareElements(prevSquares => {
+      prevSquares[i] =
+        <Square
+          key={Math.random()}
+          index={i}
+          number={Math.ceil(Math.random() * 9)}
+          clicked={true}
+        />
+    })
+
+  }
+
+  function rollSquares() {
+
+  }
+
   return (
-    <div className="square-container">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-    </div>
+    <main className="main-container">
+      <div className="title">Tenzies Game</div>
+      <div className="square-container">
+        {squareElements.map(square => {
+          return square
+        })}
+      </div>
+      <button className="roll-btn" onClick={rollSquares}>Roll</button>
+    </main>
   );
 }
 
--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,25 @@
 import React from "react";
 
-function Square() {
-  return (
-    <div className="square">1</div>
-  );
+function Square(props) {
+
+    const [clicked, setClicked] = React.useState(false)
+
+    const style = {
+        backgroundColor: clicked ? "#59e391" : "#b9bab4"
+    }
+
+    function handleClick() {
+        setClicked(prevClicked => {
+            return !prevClicked
+        })
+    }
+
+    return (
+        <div className="square" 
+        style={style}
+        onClick={handleClick}
+        >{props.number}</div>
+    );
 }
 
 export default Square;
